c47ff0d81517f536f8f81a59b7c5c5737ee80ecd
Ezazul Haq Abdul <61641801+ezazulhaq@users.noreply.github.com>
2025-10-17 03:31:34 +0000

    router added for status checks

--- main.py
+++ main.py
@@ -1,24 +1,8 @@
-from typing import Annotated
-from fastapi import FastAPI, HTTPException, Depends, status
-from sqlmodel import Session, text
-from config.database import get_db_session
-from routers import chat, quran
-
-SessionDep = Annotated[Session, Depends(get_db_session)]
+from fastapi import FastAPI
+from routers import chat, quran, status
 
 app = FastAPI()
 
+app.include_router(status.router)
 app.include_router(quran.router)
 app.include_router(chat.router)
-
-@app.get("/")
-async def status_check():
-    return {"status": "OK"}
-
-@app.get("/health/db")
-def test_db_connection(session: SessionDep):
-    try:
-        session.exec(text("SELECT 1"))
-        return {"status": "OK", "message": "Database connection successful ✅"}
-    except Exception as e:
-        raise HTTPException(status_code=status.HTTP_500_INTERNAL_SERVER_ERROR, detail=f"Database connection failed: {str(e)}")
