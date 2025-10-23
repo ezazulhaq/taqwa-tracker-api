24d2b9afc174d21e06c19801e9255ea0d5c647d9
Ezazul Haq Abdul <61641801+ezazulhaq@users.noreply.github.com>
2025-10-23 12:04:02 +0000

    configurations added for resend, jwt and rate limits

---
+++ config/email.py
@@ -0,0 +1,36 @@
+import os
+from dotenv import load_dotenv
+
+load_dotenv()
+
+class ResendConfig:
+    
+    def __init__(self, api_key, from_email, app_name, front_end_url):
+        self.__api_key = api_key
+        self.__from_email = from_email
+        self.__app_name = app_name
+        self.__front_end_url = front_end_url
+        
+    @property
+    def api_key(self):
+        return self.__api_key
+    
+    @property
+    def from_email(self):
+        return self.__from_email
+    
+    @property
+    def app_name(self):
+        return self.__app_name
+    
+    @property
+    def front_end_url(self):
+        return self.__front_end_url
+    
+
+config = ResendConfig(
+    api_key=os.getenv("RESEND_API_KEY"),
+    from_email=os.getenv("RESEND_FROM_EMAIL"),
+    app_name=os.getenv("APP_NAME"),
+    front_end_url=os.getenv("FRONT_END_URL")
+)
