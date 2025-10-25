7b8c57019040af09412be283c71c7edaab554975
Ezazul Haq Abdul <61641801+ezazulhaq@users.noreply.github.com>
2025-10-25 01:13:19 +0000

    user service included to main

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
 from fastapi import FastAPI
-from routers import auth, chat, quran, status
+from routers import auth, chat, quran, status, user
 
 app = FastAPI()
 
@@ -7,3 +7,4 @@ app.include_router(status.router)
 app.include_router(auth.router)
 app.include_router(quran.router)
 app.include_router(chat.router)
+app.include_router(user.router)
