73acbde4ccfb67a4276f0a463b72ab97b0d3b973
ezazulhaq <61641801+ezazulhaq@users.noreply.github.com>
2025-10-27 05:04:30 +0530

    renamed the service modules

---
+++ services/quran.py
@@ -0,0 +1,10 @@
+from sqlmodel import Session, select
+from entity.surah import VSurahDetails
+from model.surah import SurahDetails
+
+class QuranService:
+    
+    def get_surahs(self, surah_no: int, session: Session):
+        statement = select(VSurahDetails).where(VSurahDetails.translator_name == 'Ahmed Raza').where(VSurahDetails.surah_no == surah_no)
+        results = session.exec(statement).all()
+        return [SurahDetails.model_validate(result.model_dump()) for result in results]
