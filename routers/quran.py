24d3aed9eae775591c19a6eead130df73d0ba55e
ezazulhaq <61641801+ezazulhaq@users.noreply.github.com>
2025-10-27 05:05:52 +0530

    used Dependency layer to access quran services

--- routers/quran.py
+++ routers/quran.py
@@ -5,21 +5,23 @@ from sqlmodel import Session
 
 from config.database import get_db_session
 from model.surah import SurahDetails
-from services.surah_service import SurahService
+from services.quran import QuranService
 from starlette import status
 
 router = APIRouter(prefix="/quran", tags=["Quran Services"])
 
 SessionDep = Annotated[Session, Depends(get_db_session)]
 
+QuranDep = Annotated[QuranService, Depends()]
+
 @router.get("/surahs/{surah_no}", status_code=status.HTTP_200_OK, response_model=list[SurahDetails])
 def get_ayahs_by_surah(
     surah_no: Annotated[int, Path(ge=1, le=114, title="Surah Number", description="Surah Number")], 
-    session: SessionDep
+    session: SessionDep,
+    quran: QuranDep
     ):
     try:
-        surah_service = SurahService()
-        surahDetails: list[SurahDetails] = surah_service.get_surahs(surah_no, session)
+        surahDetails: list[SurahDetails] = quran.get_surahs(surah_no, session)
         if not surahDetails:
             raise HTTPException(status_code=status.HTTP_404_NOT_FOUND, detail="Ayahs not found")
         
