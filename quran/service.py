99e80d40b1af407eecbf0715d2d625c878b31169
Ezazul Haq Abdul <61641801+ezazulhaq@users.noreply.github.com>
2025-11-05 09:26:44 +0000

    service added to list all the surahs

--- quran/service.py
+++ quran/service.py
@@ -1,10 +1,16 @@
+from typing import List
 from sqlmodel import Session, select
-from quran.entity import VSurahDetails
-from quran.model import SurahDetails
+from quran.entity import VSurahDetails, Surah
+from quran.model import AyahDetails, SurahResponse
 
 class QuranService:
     
-    def get_surahs(self, surah_no: int, session: Session):
+    def get_surahs(self, session: Session) -> List[SurahResponse]:
+        statement = select(Surah).order_by(Surah.surah_id)
+        results = session.exec(statement).all()
+        return results
+    
+    def get_ayahs(self, surah_no: int, session: Session) -> List[AyahDetails]:
         statement = select(VSurahDetails).where(VSurahDetails.translator_name == 'Ahmed Raza').where(VSurahDetails.surah_no == surah_no)
         results = session.exec(statement).all()
-        return [SurahDetails.model_validate(result.model_dump()) for result in results]
+        return results
--- routers/quran.py
+++ routers/quran.py
@@ -4,7 +4,7 @@ from fastapi import APIRouter, Depends, HTTPException, Path
 from sqlmodel import Session
 
 from config.database import get_db_session
-from quran.model import SurahDetails
+from quran.model import AyahDetails, SurahResponse
 from quran.service import QuranService
 from starlette import status
 
@@ -14,17 +14,31 @@ SessionDep = Annotated[Session, Depends(get_db_session)]
 
 QuranDep = Annotated[QuranService, Depends()]
 
-@router.get("/surahs/{surah_no}", status_code=status.HTTP_200_OK, response_model=list[SurahDetails])
+@router.get("/surahs", status_code=status.HTTP_200_OK, response_model=list[SurahResponse])
+def get_surah_info(
+    session: SessionDep,
+    quran: QuranDep
+    ):
+    try:
+        ayahDetails: list[SurahResponse] = quran.get_surahs(session)
+        if not ayahDetails:
+            raise HTTPException(status_code=status.HTTP_404_NOT_FOUND, detail="Surahs not found")
+        
+        return ayahDetails
+    except Exception as e:
+        raise HTTPException(status_code=status.HTTP_500_INTERNAL_SERVER_ERROR, detail=f"Error fetching surahs")
+
+@router.get("/surahs/{surah_no}", status_code=status.HTTP_200_OK, response_model=list[AyahDetails])
 def get_ayahs_by_surah(
     surah_no: Annotated[int, Path(ge=1, le=114, title="Surah Number", description="Surah Number")], 
     session: SessionDep,
     quran: QuranDep
     ):
     try:
-        surahDetails: list[SurahDetails] = quran.get_surahs(surah_no, session)
-        if not surahDetails:
+        ayahDetails: list[AyahDetails] = quran.get_ayahs(surah_no, session)
+        if not ayahDetails:
             raise HTTPException(status_code=status.HTTP_404_NOT_FOUND, detail="Ayahs not found")
         
-        return surahDetails
+        return ayahDetails
     except Exception as e:
         raise HTTPException(status_code=status.HTTP_500_INTERNAL_SERVER_ERROR, detail=f"Error fetching surahs: {str(e)}")
