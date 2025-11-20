83014c67f196f0e20daba0626e94dc0f0ade975b
Ezazul Haq Abdul <61641801+ezazulhaq@users.noreply.github.com>
2025-11-20 14:53:06 +0530

    Update CORS origins to remove trailing slash

--- config/cors.py
+++ config/cors.py
@@ -1,7 +1,7 @@
 
 origins = [
     "http://localhost:4200",
-    "https://4200-firebase-taqwa-tracker-dev-1763552053170.cluster-va5f6x3wzzh4stde63ddr3qgge.cloudworkstations.dev/",
+    "https://4200-firebase-taqwa-tracker-dev-1763552053170.cluster-va5f6x3wzzh4stde63ddr3qgge.cloudworkstations.dev",
 ]
 
 methods = ["GET", "POST", "PUT", "DELETE", "OPTIONS", "PATCH"]
