109b4757fe0bd124a56fd2afb7dda68a5a48196b
Ezazul Haq Abdul <61641801+ezazulhaq@users.noreply.github.com>
2025-11-22 07:38:37 +0530

    Add new CORS origin for The Taqwa Tracker

--- config/cors.py
+++ config/cors.py
@@ -2,6 +2,7 @@
 origins = [
     "http://localhost:4200",
     "https://4200-firebase-taqwa-tracker-dev-1763552053170.cluster-va5f6x3wzzh4stde63ddr3qgge.cloudworkstations.dev",
+    "https://developer.thetaqwatracker.com",
 ]
 
 methods = ["GET", "POST", "PUT", "DELETE", "OPTIONS", "PATCH"]
